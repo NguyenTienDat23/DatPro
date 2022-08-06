c23d27c4cbc49d053a74491c1e4b76b7ccd69c31
NguyenTienDat23 <103103011+NguyenTienDat23@users.noreply.github.com>
2022-08-06 16:08:04 +0700

    hoàn thành bài thi

---
+++ GeometryExample/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    public string name;
+    public double weight;
+    public void Show()
+    {
+        Console.WriteLine("Name: " + name + ", Weight: " + weight);
+    }
+    public void Setme(string name, double weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+}
+class lion : Animal
+{
+    public lion(string name, double weight)
+    {
+        Setme(name, weight);
+        Show();
+    }
+}
+class tiger : Animal
+{
+    public tiger(string name, double weight)
+    {
+        Setme(name, weight);
+        Show();
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Lion lion = new lion("lion", 100);
+        Tiger tiger = new tiger("tiger", 200);
+    }
+}
